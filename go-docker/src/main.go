f50a3430fa60a1fe4040bfb397b85374bba213d4
ermineaweb <ermineaweb@gmail.com>
2022-01-15 22:41:00 +0100

    json

--- go-docker/src/main.go
+++ go-docker/src/main.go
@@ -4,4 +4,15 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello world")
+	err := HttpError{status: 404, message: "not found"}
+	fmt.Println(err.Error())
+}
+
+type HttpError struct {
+	status  int
+	message string
+}
+
+func (err *HttpError) Error() string {
+	return fmt.Sprintf("%d %s", err.status, err.message)
 }
---
+++ json/go.mod
@@ -0,0 +1,5 @@
+module github.com/ermineaweb/go-sandbox/json
+
+go 1.16
+
+require github.com/julienschmidt/httprouter v1.3.0 // indirect
